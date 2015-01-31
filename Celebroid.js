9e2c13c61979e36d52f515fb82306a348ddfe0d5
starcolon <pataoengineer@gmail.com>
2015-01-31 17:27:42 +0700

    ADD QUICK GUIDE & IMPLEMENT DRAFT HTTP VERBS FOR MENTOR USE

--- Celebroid.js
+++ Celebroid.js
@@ -9,24 +9,26 @@
 //-------------------------------------------------
 */
 
+var myMentor = {};
+const appVersion = 0.0001;
+const appName = 'Celebroid';
+var serviceUrl = ''; // To be assigned run-time when server starts
+
+// Initialize project dependencies
+var app = require('express')();
+var colors = require('colors');
+var jade = require('jade');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var mentor = require('./lib/mentor.js');
+
 
 (function loop(config){
-
-
-	// Initialize the dependencies
-	var app = require('express')();
-	var colors = require('colors');
-	var bodyParser = require('body-parser');
-	var mentor = require('./lib/mentor.js');
-
-	var myMentor = new mentor.mentor(config.dbName,config.dbCollectionName);
+	// Initializ the mentor object
+	myMentor = new mentor.mentor(config.dbName,config.dbCollectionName);
 
 	// Initialize the server model
 	configServer(app,bodyParser);
-
-	// Map REST verbs
-	app.get('/lesson/add/:v', httpAddLesson); // TAOTODO: On production, this should be [PUT]
-	app.get('/lesson/ls/', httpLsLesson);
 
 	// Load the current state of the lessons to the memory
 	myMentor.recall();
@@ -36,9 +38,10 @@
 	var server = app.listen(config.port, function(){
 		var host = server.address().address;
 		var port = server.address().port;
+		serviceUrl = 'http://'+host+':'+port+'/';
 
 		console.log('****************************************************'.cyan);
-		console.log('      CELEBROID starts!'.cyan );
+		console.log(('      '+appName.toUpperCase()+' starts!').toString().cyan );
 		console.log('      listening carefully at:'.cyan + (host + ':' + port).toString().green );
 		console.log('****************************************************'.cyan);
 		console.log('');
@@ -62,17 +65,70 @@ function configServer(app,bodyParser){
 	  res.header('Access-Control-Allow-Headers', 'Content-Type');
 	  next();
 	});
+
+	// Map REST parameters
+	app.param('v',function(req,resp,next,v){
+		// parameter [v] must be a JSON format
+		req.v = v;
+		return next();
+	});
+ 
+	// Map REST verbs
+	app.get('/', httpShowGuide);
+	app.get('/lesson/label/add/:v', httpAddLabel);
+	app.get('/lesson/state/add/:v', httpAddState);
+	app.get('/lesson/state/emission/:v', httpAddEmission);
+	app.get('/lessson/state/transition/:v', httpAddTransition);
+	app.get('/lesson/ls/', httpLsLesson);
 }
 
 
 
-function httpAddLesson(req,resp,next){
-	
-
+function httpShowGuide(req,resp,next){
+	// Display the quick guide document
+	fs.readFile('./html/quick-guide.jade', 'utf-8', function(error, source){
+		var data = {
+			projectname: appName,
+			projectversion: appVersion,
+			projecturl: serviceUrl
+		};
+		var html = jade.compile(source)(data);
+  		resp.send(html);
+  	});
 }
 
+
+function httpAddLabel(req,resp,next){
+	console.log('Set label :'.green);
+	console.log(req.v);
+	myMentor.setLabels(req.v);
+}
+
+
+function httpAddState(req,resp,next){
+	console.log('Set state :'.green);
+	console.log(req.v);
+	myMentor.setState(req.v);
+}
+
+
+function httpAddEmission(req,resp,next){
+	console.log('Set emission :'.green);
+	console.log(req.v);
+	myMentor.setEmission(req.v);
+}
+
+
+function httpAddTransition(req,resp,next){
+	console.log('Set transition :'.green);
+	console.log(req.v);
+	myMentor.setTransition(req.v);
+}
 
 
 function httpLsLesson(req,resp,next){
+	// Print out the lesson
+	myMentor.showLessons();
+	resp.send(myMentor.lessons);
 }
 
